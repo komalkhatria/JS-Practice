eb7df8baa397ede32e4384f98a739f7abb3210b1
komal <komalkhatria29@gmail.com>
2025-01-03 09:23:17 +0530

    datatypes in javascript

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict";   // treats entire code as newer version of javascript
+
+// alert("Welcome");   // alert is not defined, we are using nodejs not browser 
+
+let name= "Nick";   // string
+let age= 18;    // number
+let isAdult= true;  // boolean
+
+console.table([name, age, isAdult]);
+
+console.log(typeof(isAdult));   // typeof returns datatype
+
+
+// number => integer values
+// bigint => big integer values
+// string => '' or ""(preferred)
+// boolean => true / false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique
+
+// object =>
+
+console.log(typeof(null));  // object
+console.log(typeof(undefined));    // undefined 
